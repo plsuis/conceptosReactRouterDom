c77690ce30764e44e3660a8dc63eb0c50c95ba28
suplis <israel.surribas.planas@gmail.com>
2024-02-16 20:18:42 +0100

    Rutas relativas

--- Analizando.md
+++ Analizando.md
@@ -31,3 +31,33 @@
     <Route path="/componenteruta"/>
     ```
     - Agrego os links adecuados no compoñente `Links`
+
+8. Comentamos no compoñente Links os links asociados ás rutas e o propio de BrowserRouter
+
+9. Observade cando colocamos a etiqueta `<nav>`, esta deberá estar contida sobre os `Links` co compoñente `Outlet` fora.
+    - Observade se colocamos `Outlet` dentro da etiqueta `nav` na barra de ferramentas do navegador
+
+> Isto implica que deberemos observar sobre que etiquetas envolvemos os Links, e deixar fora o componente `Outlet`
+
+10. O uso de `Outlet` é moi importante de cara as rutas relativas.
+
+```javascript
+function ComponenteVentas(){
+    const {id} = useParams()
+    console.log('estou en relativa2 e o id é: ',id)
+    return(<div className="componentes">
+        <h3>Esta é unha ruta relativa2</h3>
+        <div>o id é {id}</div>
+       
+       <Outlet />
+    </div>)
+}
+```
+    - Se o quitamos non veríamos os fillos asociados:
+
+    ```javascript
+            <Route path="ventas" element={<ComponenteVentas />}>
+                <Route path=":id" element={<ComponenteRutaRelativa />} />
+                <Route path=":id/ruta0" element={<h1>componente anidado</h1>}/>
+            </Route>
+    ```
--- src/componentes/Rutas.jsx
+++ src/componentes/Rutas.jsx
@@ -1,21 +1,31 @@
-import { Outlet,Route,Routes,Link } from "react-router-dom";
+import { Outlet,Route,Routes,Link, useParams } from "react-router-dom";
 
 
 function Layout(){
-    return (
-    <div><h1>O Layout</h1>
-        <Link to="/ruta0">Ruta 0</Link>
-        <Link to="/componenteruta">Componente Ruta</Link>
-        <Link to="/ventas/5">Ruta relativa</Link>
-        <Outlet />
-    </div>
+    const {id} = useParams()
+    console.log('estou en layout id: ',id)
+    return (<>
+        <nav>
+            <Link to="/ruta0">Ruta 0</Link>
+            <Link to="/componenteruta">Componente Ruta</Link>
+            <Link to="contactos/3">Ruta relativa con contactos</Link>
+            <Link to="/ventas">Ruta con ventas</Link>
+            <Link to="/ventas/5">Ruta relativa con ventas</Link>
+            <Link to="/ventas/8/ruta0">Ruta relativa con ventas</Link>
+        </nav>
+   
+    <Outlet />
+        </>
     )
 }
 
+
+
+
 function Dashboard() {
     return (
-      <div>
-        <h1>Dashboard</h1>
+      <div className="componentes">
+        <h5>Dashboard</h5>
 {/*         <Outlet />
  */}      </div>
     );
@@ -23,36 +33,56 @@ function Dashboard() {
 
 function ComponenteAnidado(){
     return (
-        <div>
+        <div className="componentes">
             <h2>Compoñente anidado</h2>
+            
         </div>
     )
 }
 
 function ComponenteRutaRelativa(){
-    return(<div>
+    const {id} = useParams()
+    console.log('estou en ruta relativa e o id é: ',id)
+    return(<div className="componentes">
         <h3>Esta é unha ruta relativa</h3>
-        <Outlet />
+        <div>o id é {id}</div>
+       
+    </div>)
+}
+function ComponenteVentas(){
+    const {id} = useParams()
+    console.log('estou en relativa2 e o id é: ',id)
+    return(<div className="componentes">
+        <h3>Componente VENTAS</h3>
+        <div>o id é {id}</div>
+       
+       <Outlet />
     </div>)
 }
 function ComponenteRuta(){
-    return (<div><h3>Outro compoñente</h3></div>)
+    return (<div className="componentes"><h3>Outro compoñente</h3></div>)
 }
+function Componente404(){
+    return (<h1>404</h1>)
+}
+
 export default function Rutas(){
 
     return(
     <Routes>
         <Route path="/" element={<Layout />} >
-        {/* <Route index element={<Dashboard />}>
-            <Route path="ruta0" element={<ComponenteAnidado />}></Route>
-        </Route> */}
-        <Route index element={<Dashboard />} />
+          
+            <Route index element={<Dashboard />} />
+                
+            <Route path="contactos/:id" element={<ComponenteRutaRelativa />} />
             
-        <Route path="contactos/:id" element={<ComponenteRutaRelativa />} />
-        <Route path="ventas/:id" element={<ComponenteRutaRelativa />}>
-            <Route path="ventas/:id/ruta0" element={<ComponenteAnidado />}/>
-        </Route>
-        <Route path="componenteruta"/> 
+            <Route path="ventas" element={<ComponenteVentas />}>
+                <Route path=":id" element={<ComponenteRutaRelativa />} />
+                <Route path=":id/ruta0" element={<h1>componente anidado</h1>}/>
+            </Route>
+            <Route path="componenteruta" element={<ComponenteRuta />}/>
+            <Route path="ruta0" element={<ComponenteAnidado />} /> 
+            <Route path="*" element={<Componente404 />}></Route>
         </Route>
     </Routes>
     )
