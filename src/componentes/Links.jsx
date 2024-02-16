f29403642609d4adedbabbd123992339f7fca06e
suplis <israel.surribas.planas@gmail.com>
2024-02-16 15:11:33 +0100

    Agrego atributo index

--- src/componentes/Links.jsx
+++ src/componentes/Links.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 
 export default function Links() {
   return (
-    <div>
-      <h1>Rutas</h1>
-      <Link to="/ruta0">Ruta 0</Link>
-      <Link to="/componenteruta">Componente Ruta</Link>
+    <div>Estou en Links
+      {/* <h1>Rutas</h1> */}
+      {/* <Link to="/ruta0">Ruta 0</Link>
+      <Link to="/componenteruta">Componente Ruta</Link> */}
     </div>
   );
 }
--- src/componentes/Rutas.jsx
+++ src/componentes/Rutas.jsx
@@ -1,4 +1,16 @@
-import { Outlet,Route,Routes } from "react-router-dom";
+import { Outlet,Route,Routes,Link } from "react-router-dom";
+
+
+function Layout(){
+    return (
+    <><h1>O Layout</h1>
+        <Link to="/ruta0">Ruta 0</Link>
+        <Link to="/componenteruta">Componente Ruta</Link>
+        <Link to="/ventas/5">Ruta relativa</Link>
+        <Outlet />
+    </>
+    )
+}
 
 function Dashboard() {
     return (
@@ -30,14 +42,17 @@ export default function Rutas(){
 
     return(
     <Routes>
-        <Route path="/" element={<Dashboard />}>
-            <Route path="/ruta0" element={<ComponenteAnidado />}></Route>
+        <Route path="/" element={<Layout />} />
+        {/* <Route index element={<Dashboard />}>
+            <Route path="ruta0" element={<ComponenteAnidado />}></Route>
+        </Route> */}
+        <Route index element={<Dashboard />} />
+            
+        <Route path="contactos/:id" element={<ComponenteRutaRelativa />} />
+        <Route path="ventas/:id" element={<ComponenteRutaRelativa />}>
+            <Route path="ventas/:id/ruta0" element={<ComponenteAnidado />}/>
         </Route>
-        <Route path="/contactos/:id" element={<ComponenteRutaRelativa />} />
-        <Route path="/ventas/:id" element={<ComponenteRutaRelativa />}>
-            <Route path="/ventas/:id/ruta0" element={<ComponenteAnidado />}/>
-        </Route>
-        <Route path="/componenteruta"/>
+        <Route path="componenteruta"/>
     </Routes>
     )
 }
