757df490679d8daaa325db2ed2d79305d0481cc5
suplis <israel.surribas.planas@gmail.com>
2024-02-18 20:24:06 +0100

    Rutas absolutas

--- src/componentes/Rutas.jsx
+++ src/componentes/Rutas.jsx
@@ -1,22 +1,30 @@
-import { Outlet,Route,Routes } from "react-router-dom";
+import { Outlet,Route,Routes,Link } from "react-router-dom";
 
 function Dashboard() {
     return (
       <div>
         <h1>Dashboard</h1>
-  
-        {/* This element will render either <DashboardMessages> when the URL is
-            "/messages", <DashboardTasks> at "/tasks", or null if it is "/"
-        */}
-        <Outlet />
+        <Link to="/">Home</Link>
+        <Link to="/logueo">Logueo</Link>
+        <Link to="/rexistro">Rexistro</Link>
       </div>
     );
   }
+
+function Logueo(){
+    return(<>Estou en logueo</>)
+}
+
+function Rexistro(){
+    return(<>Estou en Rexistro</>)
+}
 export default function Rutas(){
 
     return(
     <Routes>
         <Route path="/" element={<Dashboard />}/>
+        <Route path="logueo" element={<Logueo />}/>
+        <Route path="rexistro" element={<Rexistro />} />
     </Routes>
     )
 }
